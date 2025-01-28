cb36d38032b400cce53bd5638851dcfa23aacfbe
nadendlachervi <chervinadendla@gmail.com>
2025-01-28 23:22:33 +0530

    Create App.js

---
+++ ai-ecosystem-for-kids/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AI Ecosystem for Kids</h1>
+        <p>Welcome to our learning platform!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
